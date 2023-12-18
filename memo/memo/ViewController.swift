498b07d4e9c6fffce2dae668a5263d1bb48a50f7
DONOTINTO <goods99j@gmail.com>
2023-12-18 16:13:53 +0900

    add_memo project

---
+++ memo/memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by 이중엽 on 12/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
