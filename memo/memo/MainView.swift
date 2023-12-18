49340e76e565dee8d61a866eda0492d8ff66ff25
DONOTINTO <goods99j@gmail.com>
2023-12-18 16:46:10 +0900

    update_view enterbutton

--- memo/memo/MainView.swift
+++ memo/memo/MainView.swift
@@ -6,7 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainView: UIView {
-
+    
+    let enterButton = UIButton()
+    
+    func makeUI() {
+        self.backgroundColor = .white
+        self.addSubview(enterButton)
+        
+        enterButton.setTitle("Enter", for: .normal)
+        enterButton.setTitleColor(.white, for: .normal)
+        enterButton.backgroundColor = .black
+        enterButton.layer.cornerRadius = 10
+        
+        enterButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.width.equalTo(120)
+            $0.centerX.equalTo(self.safeAreaLayoutGuide.snp.centerX)
+            $0.centerY.equalTo(self.safeAreaLayoutGuide.snp.centerY)
+        }
+    }
 }
--- memo/memo/ViewController.swift
+++ memo/memo/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let mainView = MainView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view = mainView
+        
+        mainView.makeUI()
+        setAction()
+    }
+    
+    func setAction() {
+        mainView.enterButton.addTarget(self, action: #selector(addEnterButtonClicked), for: .touchUpInside)
     }
 
-
+    @objc func addEnterButtonClicked() {
+        
+    }
 }
 
