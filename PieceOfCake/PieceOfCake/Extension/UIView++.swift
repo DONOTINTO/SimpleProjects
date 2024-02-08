de7f6959aaf5e1075ca7f3fd6207dd7ae763ce63
DONOTINTO <goods99j@gmail.com>
2024-02-08 15:00:17 +0900

    Update - identifier

---
+++ PieceOfCake/PieceOfCake/Extension/UIView++.swift
@@ -0,0 +1,14 @@
+//
+//  UIView++.swift
+//  PieceOfCake
+//
+//  Created by 이중엽 on 2/8/24.
+//
+
+import UIKit
+
+extension UIView {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
